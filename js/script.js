a85b29ed0958426a77fc0f40e53825a18020a92c
FonesAriel <FonesArielDW@gmail.com>
2024-01-15 15:34:25 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Esto proporciona un desplazamiento suave
+    });
+}
+
+// Asigna el evento de clic al enlace de "INICIO"
+document.querySelector('.navigation-link[href="#inicio"]').addEventListener('click', function (e) {
+    e.preventDefault();
+    scrollToTop();
+});
+
+
+function scrollToContent() {
+    // Encuentra el elemento con la clase 'about-us' y desplázate hacia él
+    document.querySelector('.about-us').scrollIntoView({ behavior: 'smooth' })
+}
+
+// Función para desplazarse suavemente a las secciones al hacer clic en los enlaces de la barra de navegación
+document.querySelectorAll('.navigation-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
